105ea3cabd34f3c8ab5d3e5e7914d1cecc8383e2
hyebin <bin930827@gmail.com>
2023-08-23 18:14:15 +0900

    slide

---
+++ script/common.js
@@ -0,0 +1,20 @@
+const sub = document.querySelectorAll('.sub1')
+const gnb = document.querySelectorAll('.gnb > li')
+const nav_bg = document.querySelector('.nav_bg')
+console.log(sub, gnb, nav_bg)
+sub_func('none')
+function sub_func (status){
+    nav_bg.style.display = status
+    for(let i of sub){i.style.display= status}
+}
+for(let i of gnb){
+    console.log(i)
+    i.addEventListener('mouseover', function(){
+        sub_func('block')
+        i.firstElementChild.style.color='#F87D18'
+    })
+    i.addEventListener('mouseout',function(){
+        sub_func('none')
+        i.firstElementChild.style.color='#000'
+    })
+}
